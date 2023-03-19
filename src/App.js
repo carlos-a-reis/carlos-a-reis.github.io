59ab78e0c15611f7de0b6ba21ce54fb1407b01da
Carlos Augusto dos Reis <hadqen@gmail.com>
2023-03-19 15:20:39 -0300

    feat: adiciona todas as rotas do projeto

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
+import About from './pages/About';
 import Home from './pages/Home';
+import Projects from './pages/Projects';
+import Skills from './pages/Skills';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={ <Home /> }/>
+      <Route path="/projetos" element={ <Projects /> }/>
+      <Route path="/skills" element={ <Skills /> }/>
+      <Route path="/sobre" element={ <About /> }/>
     </Routes>
   );
 }
