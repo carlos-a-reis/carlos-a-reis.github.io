0fa49f11954c57087640ef18fc5df94774b78cc0
Carlos Augusto dos Reis <hadqen@gmail.com>
2023-03-19 15:17:02 -0300

    feat: adiciona o footer do projeto

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,29 @@
+import React from 'react';
+
 function Footer() {
-  return(
-    <div>
-    </div>
+  return (
+    <footer>
+      <a 
+        href="https://linkedin.com/in/carlos-augusto-dos-reis"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <i>LINKEDIN</i>
+      </a>
+      <a href="mailto:carlos.aug.dosreis@gmail.com" target="_blank" rel="noopener noreferrer">
+      <i>GMAIL</i>
+      </a>
+      <a href="https://github.com/carlos-a-reis" target="_blank" rel="noopener noreferrer">
+        <i>GITHUB</i>
+      </a>
+      <a
+        href="https://api.whatsapp.com/send/?phone=27992037517"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <i>WHATSAPP</i>
+      </a>      
+    </footer>
   );
 }
 
