2be78ea053ad3fd8b1551f4a3479ae2e7eedd7ce
Carlos Augusto dos Reis <hadqen@gmail.com>
2023-03-19 14:21:33 -0300

    feat: adiciona a barra de navegação superior do site

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
+import { Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+
 function App() {
   return (
-    <div>
-    </div>
+    <Routes>
+      <Route path="/" element={ <Home /> }/>
+    </Routes>
   );
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,30 @@
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
 function Header() {
+  const [navDisplay, setNavDisplay] = useState('none');
+  const navigate = useNavigate();
+
+  const setDisplayVisibility = () => {
+    if (navDisplay === 'none') {
+      setNavDisplay('flex');
+    } else {
+      setNavDisplay('none');
+    }
+  }
+
   return(
-    <div>
-    </div>
+    <header>
+      <i onClick={() => setDisplayVisibility()}>menu</i>
+      <nav style={{display: navDisplay}}>
+        <ul>
+          <li onClick={() => navigate("/")}>HOME</li>
+          <li onClick={() => navigate("/projetos")}>PROJETOS</li>
+          <li onClick={() => navigate("/skills")}>SKILLS</li>
+          <li onClick={() => navigate("/sobre")}>SOBRE</li>
+        </ul>
+      </nav>
+    </header>
   );
 }
 
