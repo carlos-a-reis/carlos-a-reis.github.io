0369f02857e1d1d707609cdb7fe4ec9562bc9711
Carlos <hadqen@gmail.com>
2023-03-20 23:38:00 -0300

    feat: adiciona o footer responsivo da aplicação

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,29 +1,52 @@
 import React from 'react';
+import * as S from './styles';
+import linkedinIcon from '../../assets/icons/linkedin.svg';
+import gmailIcon from '../../assets/icons/gmail.svg';
+import githubIcon from '../../assets/icons/github.svg';
+import whatsappIcon from '../../assets/icons/whatsapp.svg';
 
 function Footer() {
   return (
-    <footer>
-      <a 
-        href="https://linkedin.com/in/carlos-augusto-dos-reis"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <i>LINKEDIN</i>
-      </a>
-      <a href="mailto:carlos.aug.dosreis@gmail.com" target="_blank" rel="noopener noreferrer">
-      <i>GMAIL</i>
-      </a>
-      <a href="https://github.com/carlos-a-reis" target="_blank" rel="noopener noreferrer">
-        <i>GITHUB</i>
-      </a>
-      <a
-        href="https://api.whatsapp.com/send/?phone=27992037517"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <i>WHATSAPP</i>
-      </a>      
-    </footer>
+    <S.Footer>
+      <S.Contacts>
+        <li>
+          <a
+            href='https://linkedin.com/in/carlos-augusto-dos-reis'
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            <S.Img src={linkedinIcon} />
+          </a>
+        </li>
+        <li>
+          <a
+            href='mailto:carlos.aug.dosreis@gmail.com'
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            <S.Img src={gmailIcon} />
+          </a>
+        </li>
+        <li>
+          <a
+            href='https://github.com/carlos-a-reis'
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            <S.Img src={githubIcon} />
+          </a>
+        </li>
+        <li>
+          <a
+            href='https://api.whatsapp.com/send/?phone=27992037517'
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            <S.Img src={whatsappIcon} />
+          </a>
+        </li>
+      </S.Contacts>
+    </S.Footer>
   );
 }
 
---
+++ src/components/Footer/styles.js
@@ -0,0 +1,31 @@
+import styled from 'styled-components';
+
+export const Footer = styled.footer`
+  display: flex;
+  justify-content: center;
+  align-content: center;
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+  height: 58px;
+  background-color: #301E67;
+
+  @media (min-width: 760px) {
+
+  }
+
+  @media (min-width: 1440px) {
+    height: 78px;
+  }
+`;
+
+export const Contacts = styled.ul`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 273px;
+`;
+
+export const Img = styled.img`
+  width: 24px;
+`;
