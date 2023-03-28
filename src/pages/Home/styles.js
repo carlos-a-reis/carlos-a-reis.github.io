2844896cb35a0eb89e63745c959cdeceb48bd393
Carlos <hadqen@gmail.com>
2023-03-28 18:22:22 -0300

    fix: corrige o bug de tamanho da tela no mobile

--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -2,11 +2,12 @@ import styled from 'styled-components';
 import background from '../../assets/images/background.jpg';
 
 export const Main = styled.main`
+  flex: 1;
   display: flex;
   flex-direction: column;
   justify-content: center;
   height: 100vh;
-  background-color: #03001C;
+  background-color: #03001c;
 `;
 
 export const BackGround = styled.div`
